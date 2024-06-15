56d65f1b3fdd665afe68250c22b1cbe09896a0da
MuhammadDzakiFawwazArdika <dzakwas01@gmail.com>
2024-06-15 20:48:27 +0700

    fitur web.php

--- routes/web.php
+++ routes/web.php
@@ -6,35 +6,7 @@ use App\Http\Controllers\JabatanController;
 use App\Http\Controllers\CutiController;
 use App\Http\Controllers\KaryawanController;
 
-// route login/logout
-Route::controller(LoginController::class)->prefix('login')->group(function() {
-    Route::get('/', 'login')->name('login');
-    Route::post('/', 'actionlogin')->name('actionlogin');
+//asdasdajsd
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout');
-
-//route jabatan
-Route::get('/', [JabatanController::class,'index'])->name('jabatan.read');
-Route::get('/create', [JabatanController::class,'create'])->name('jabatan.create');
-Route::post('/create-proses', [JabatanController::class,'store'])->name('jabatan.createProses');
-Route::get('/edit/{id}', [JabatanController::class,'edit'])->name('jabatan.edit');
-Route::put('/edit-proses/{id}', [JabatanController::class,'update'])->name('jabatan.update');
-Route::delete('/delete/{id}', [JabatanController::class,'destroy'])->name('jabatan.delete');
-Route::get('/search', [JabatanController::class,'search'])->name('jabatan.search');
-
-//route cuti
-Route::get('/cuti', [CutiController::class,'index'])->name('cuti.read');
-Route::get('/cuti/create', [CutiController::class,'create'])->name('cuti.create');
-Route::post('/cuti/create-proses', [CutiController::class,'store'])->name('cuti.createProses');
-Route::get('/cuti/edit/{id}', [CutiController::class,'edit'])->name('cuti.edit');
-Route::put('/cuti/edit-proses/{id}', [CutiController::class,'update'])->name('cuti.update');
-Route::delete('/cuti/delete/{id}', [CutiController::class,'destroy'])->name('cuti.delete');
-
-// route karyawan
-Route::get('/karyawans', [KaryawanController::class,'index'])->name('karyawans.index');
-Route::get('/karyawans/create', [KaryawanController::class,'create'])->name('karyawans.create');
-Route::post('/karyawans/create-proses', [KaryawanController::class,'store'])->name('karyawans.store');
-Route::get('/karyawans/edit/{id}', [KaryawanController::class,'edit'])->name('karyawans.edit');
-Route::put('/karyawans/edit-proses/{id}', [KaryawanController::class,'update'])->name('karyawans.update');
-Route::delete('/karyawans/delete/{id}', [KaryawanController::class,'destroy'])->name('karyawans.delete');
